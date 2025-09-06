57fc625a5bb84604be0814af49c48fc675cf7cf4
idahm618 <idahm618@gmail.com>
2025-09-06 15:34:06 +0200

    Update espn.js

--- api/espn.js
+++ api/espn.js
@@ -2,7 +2,7 @@
 
 const LEAGUES = [
   'eng.1', 'esp.1', 'ita.1', 'ger.1', 'fra.1', 'fifa.worldq.caf', 'fifa.worldq.uefa',
-  'eng.3', 'ned.1', 'por.1', 'eng.2', 'bra.1',
+  'eng.3', 'ned.1', 'por.1', 'eng.2', 'bra.1', 'fifa.worldq.uefa',
   'arg.copa_lpf', 'uefa.europa_qual', 'uefa.champions_qual', 'fifa.worldq.conmebol', 'fifa.worldq.afc',
   'ind.1', 'uefa.nations', 'uefa.champions', 'uefa.europa', 'uefa.conference_qual',
   'fifa.world', 'conmebol.libertadores', 'nga.1', 'caf.nations',
